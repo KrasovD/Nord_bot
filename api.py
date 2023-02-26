baac5cd9cdee3ca60602cfac8ac8209387cd64f1
Dmitry Karasev <krasov.music@gmail.com>
2023-02-26 20:10:39 +0300

    Добавил функции в API (создание клиента), FSM вход

--- api.py
+++ api.py
@@ -3,52 +3,93 @@ import config
 import json
 import qrcode
 from pprint import pprint
+import datetime
+
+
+class Customer():
+    '''Базовая информация об клиентах'''
+
+    def __init__(self, id = None, accounts = None, type = None, date = None, tokens = None, firstName = '', lastName = '', **kwargs):
+        self.id = id
+        if (accounts != None): self.available = accounts[0]['accountBalance']['available']
+        self.name = firstName + lastName
+        self.type = type
+        self.tokens: list = tokens
+        self.date = date
 
 
 class Api():
     '''Выгрузка данных из CRM об клиента и бонусах'''
+
     headers = {
         'Connection': 'keep-alive',
         'Content-Type': 'application/json',
     }
-    url = 'https://{login}.quickresto.ru/platform/online/'.format(login=config.login)
+    URL = 'https://{login}.quickresto.ru/platform/online/'.format(login=config.login)
 
-    def __init__(self, number):
+    def __init__(self, number): 
         self.number = number # Номер телефона клиента
 
+
+    def datetime_format(self, date):
+        timedelta = datetime.timedelta(hours=3)
+        return (datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ') + timedelta).strftime('%d.%m %H:%M')
+
+    def json_format(self, data):
+        return str(data).replace('\'', '"').replace(' ', '').encode('utf-8')
+    
     def post(self, url, data):
         return requests.post(url=url, auth=(config.login, config.password), headers=self.headers, data=data).json()
 
     def client_info(self):
-        '''Выгрузка базовой информации о клиенте'''
-        url = self.url + 'bonuses/customerInfo'
-        data = '{"customerToken":{"type":"phone","entry":"manual","key":"%s"}}' %self.number
-        json_data = self.post(url, data)
-        return Customer(**json_data)
-        #print(response['accounts'][0]['accountBalance']['available'])
+        '''Выгрузка базовой информации о клиенте  (поиск по номеру телефона)'''
+
+        url = self.URL + 'bonuses/customerInfo'
+        data = {
+            "customerToken":{
+                "type": "phone",
+                "entry": "manual",
+                "key": self.number
+            }
+        }
+        return Customer(**self.post(url, self.json_format(data)))
 
     def format_history(self, regTime, type, amount):
+        '''Шаблон истории начисления\списания баллов'''
+
         if type == 'DEBIT_CONFIRMATION':
             type = 'Списание'
         elif type == 'CREDIT': 
             type = 'Начисление'
         else: return False
 
-        return 'Дата: {}\n{}: {} баллов'.format(regTime, type, amount )
+        return 'Дата: {}. {}: {} баллов'.format(self.datetime_format(regTime), type, amount )
 
     def client_history(self):
         '''История транзакций в бонусной системе клиента'''
-        url = self.url + 'bonuses/operationHistory'
-        data = '{"customerToken":{"type":"phone","entry":"manual","key":"%s"}, "accountType":{"accountGuid":"bonus_account_type-1"}}' % self.number
-        return [self.format_history(data['regTime'], data['type'], data['amount'])  for data in self.post(url, data)['transactions']]
+
+        url = self.URL + 'bonuses/operationHistory'
+        data = {
+            "customerToken": {
+                "type": "phone",
+                "entry": "manual",
+                "key": number
+            }, 
+            "accountType": {
+                "accountGuid": "bonus_account_type-1"
+            }
+        }
+        return [self.format_history(data['regTime'], data['type'], data['amount'])  for data in self.post(url, self.json_format(data))['transactions']]
 
     def save_json(self, response):
         '''Сохранение данных из API в json формате'''
+
         with open('data.json', 'w') as base:
             json.dump(response, base)
 
     def qr_code(self):
         '''Формирование QR кода'''
+
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -60,19 +101,48 @@ class Api():
         img = qr.make_image(fill_color="black", back_color="white")
         print(img.save('%s.png' % self.number))
         return open('%s.png' %self.number, 'rb')
+    
 
+class CustomerOperation(Api):
+    '''Операции с клиентсикими обьектами'''
 
-class Customer(Api):
-    def __init__(self, id = None, accounts = None, type = None, date = None, token = None, firstName = None, lastName = None, **kwargs):
-        self.id = id
-        if (accounts != None): self.available = accounts[0]['accountBalance']['available']
-        self.name = firstName + lastName
-        self.type = type
-        self.tokens: list = token
-        self.date = date
+    def createObject(self, firstName='', lastName='', number=''):
+        url = self.URL + 'bonuses/createCustomer'
+        data = {
+            'firstName': firstName,
+            'lastName': lastName,
+            #'dateOfBirth': datetime, #'1990-03-07T19:00:00.000Z'
+            'tokens': [{
+                'type': 'phone',
+                'entry': 'manual',
+                'key': number
+                },{
+                'type': 'card',
+                'entry': 'barCode',
+                'key': number
+                }  
+            ],
+        }
+
+        return self.post(url, self.json_format(data))
+
+    def updateObject(self):
+        params = {
+            "moduleName": 'crm.customer',
+            "ownerContextId": 'ownerContextId',
+            "ownerContextClassName": 'ownerContextClassName',
+            "parentContextId": 'parentContextId',
+            "parentContextClassName": 'parentContextClassName'
+        }
+
+        json_data = object.get_json_object()
+
+        return self.post(self.URL+'api/update', parameters=params, json_data=json_data)
+
          
 
 
 if __name__ == '__main__':
-    api = Api('79969290700')
-    pprint(api.client_history())
+    number = '736389'
+    create = CustomerOperation(number)
+    pprint(create.createObject('тест', 'тест', number))
--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils import executor
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 import config
 import api
@@ -8,22 +11,116 @@ import api
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=config.TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 number = None
 
 @dp.message_handler(commands=['start'])
 async def show_hello(message: types.Message):
     '''Сообщение при старте бота'''
+
+    key1 = types.InlineKeyboardButton(text='Вход', callback_data='login')
+    key2 = types.InlineKeyboardButton(text='Регистрация', callback_data='registration')
+    keyboard = types.InlineKeyboardMarkup().add(key1, key2)
     await message.answer(
             text='''
-            Привет, {}!
-            Введите ваш номер для идентификация в нашей системе лояльности (начиная с 7)
-            '''.format(message.chat.first_name)
+Здраствуйте, {}!
+Добро пожаловать в систему лояльности Kos.Place.
+Мы начисляем 5% с каждого чека на ваш бонусный счет,
+и в дальнейшем можем списать до 50% из суммы чека из
+ваших бонусов.'''.format(message.chat.first_name)
             )
+    await message.answer(
+        text='''
+Если вы уже зарегестрированы у нас в бонусной программе,
+нажмите вход и введите номер, который указывали при регистрации.
+Если вы впервые у нас, то нажмите регистрация
+и введите ваш номер или любой другой индетификационный номер
+        ''',
+        reply_markup=keyboard
+        )
 
 
-@dp.message_handler(content_types='text')
+@dp.callback_query_handler(lambda call: call.data)
+async def call_info(call: types.CallbackQuery):
+    if call.data == 'login':
+        await Login.check.set()
+        await bot.send_message(
+            chat_id=call.from_user.id, 
+            text='''
+            Ваш номер: (Пример: 79874352121)
+            '''
+        )
+        
+    if call.data == 'registration':
+        pass
+    if call.data == 'help':
+        pass
+
+class Registration(StatesGroup):
+    number = State()
+    name = State()
+    birthday = State()
+    finish = State()
+
+# Авторизация
+class Login(StatesGroup):
+    registration = State() # Регистрация номого пользователя
+    check = State()  # Проверка номера в БД
+    again = State()
+    finish = State()
+
+
+
+@dp.message_handler(state=Login.check)
+async def process_name(message: types.Message, state: FSMContext):
+    """
+    Проверка в БД QuickResto номера или индетификационного номера
+    """
+    customer  = api.Api(message.text)
+    info = customer.client_info()
+    if info.id:
+        # новая сфм с функциями и запись id пользователя в свою БД
+        await message.answer(text='{} у вас {} баллов'.format(info.name, info.available))
+        await state.finish()
+    else:
+        await Login.again.set()
+        await message.answer(
+            text='Вы не зарегистрированы или ввели номер не верно. Попробуйте еще раз'
+        )
+
+
+@dp.message_handler(state=Login.again)
+async def process_name(message: types.Message, state: FSMContext):
+    customer  = api.Api(message.text)
+    info = customer.client_info()
+    if info.id:
+        await message.answer(text='{} у вас {} баллов'.format(info.name, info.available))
+        await state.finish()
+    else:
+        key1 = types.InlineKeyboardButton(text='Служба поддержки', callback_data='help')
+        key2 = types.InlineKeyboardButton(text='Регистрация', callback_data='registration')
+        keyboard = types.InlineKeyboardMarkup().add(key1, key2)
+        await state.finish()
+        await message.answer(
+            text='Не могу найти, возможно Вас нет в программе лояльности',
+            reply_markup=keyboard
+        )
+
+dp.callback_query_handler(lambda call: call.data, state=Login.finish)
+async def call_info(call: types.CallbackQuery, state: FSMContext):
+    '''
+    Перехват нажатия кнопки
+    '''
+    
+    if call.data == 'help':  # отправка в службу поддержки
+        await bot.send_message(call.from_user.id, 'Служба поддержки в разработке')
+    if call.data == 'registration':
+        await Registration.number
+    
+
+'''@dp.message_handler(content_types='text')
 async def command(message:types.Message):
     if (message.text).isdigit():
         if api.Customer(message.text):
@@ -42,16 +139,8 @@ async def command(message:types.Message):
     if message.text == 'history':
         await message.answer('{}'.format(client.client_history()))
     if message.text == 'qr_code':
-        await message.answer_photo(client.qr_code())
+        await message.answer_photo(client.qr_code())'''
 
-
-@dp.callback_query_handler(lambda call: call.data)
-async def call_info(call: types.CallbackQuery):
-    if call.data == 'yes_reg':
-        await bot.send_photo(chat_id=call.from_user.id, photo=client.qr_code())
-        
-    if call.data == 'no_reg':
-        await bot.send_message(chat_id=call.from_user.id, text='Ну ладно =(')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
